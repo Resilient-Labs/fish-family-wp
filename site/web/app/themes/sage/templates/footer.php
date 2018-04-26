fc8480bd5f24e45db8da0d3edc12dd4cc8f761d1
Muigai <muigaiunaka@gmail.com>
2018-04-26 00:01:17 -0400

    with updates from FFF

--- site/web/app/themes/sage/templates/footer.php
+++ site/web/app/themes/sage/templates/footer.php
@@ -1,9 +1,25 @@
 <footer>
-  <h3>
+  <section>
+    <div>
     <a class="brand logotype" href="<?= esc_url(home_url('/')); ?>">
       <?php echo bloginfo($show = 'name'); ?>
     </a>
-  </h3>
+    </div>
+    <div>
+      <a class="color-light-gray" href="mailto:<?php echo the_author_meta( 'user_email', 1 );?>"> 
+        <?php echo the_author_meta( 'user_email', 1 );?>
+      </a>
+      <p class="color-light-gray">
+      <?php $phone = get_the_author_meta( 'user_url', 1 ); ?>
+      </p>
+      <p class="color-light-gray">
+      <?php echo str_replace('http://', '',  $phone);?>
+      </p>
+      <p class="color-light-gray">
+       <?php echo the_author_meta( 'description', 1 );?>
+      </p>
+    </div>
+  </section>
   <nav>
     <?php
       $args = array('menu_class' => 'menu', 'container' => false);
